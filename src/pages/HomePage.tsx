b4fe90afdd922f7f311a036338cd5246f1312154
Rosen Yorgov <yorgov.h.r@gmail.com>
2025-07-31 22:38:42 +0300

    wip - styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -25,10 +25,6 @@ enum FormData {
 
 const HomePage = () => {
   const [renderForm, setRenderForm] = useState<FormData>(FormData.Header);
-  //TODO: have these
-  //choose a template
-  //forms for input + upload of image (optional depending on the template they choose)
-  //next button => leads a page with already populated data
 
   const renderDataForm = () => {
     switch (renderForm) {
