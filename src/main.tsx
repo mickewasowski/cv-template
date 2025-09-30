ec5880c2c882e486cfc091b2f3aea585233526cb
Rosen Yorgov <yorgov.h.r@gmail.com>
2025-09-30 22:02:37 +0300

    installed material ui and added theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,18 @@ import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store/store.ts";
 import App from "./App.tsx";
 import "./index.css";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./material-ui/theme";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+       </ThemeProvider>
       </PersistGate>
     </Provider>
   </StrictMode>,
